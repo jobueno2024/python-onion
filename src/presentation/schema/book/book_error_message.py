c0dff11a8ee4141e98c2b64f0cc78cbd0080e35d
jobueno2024 <job.ueno.2024@gmail.com>
2024-08-25 14:59:01 +0000

    presentationを追加

---
+++ src/presentation/schema/book/book_error_message.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+
+from src.domain.book import (
+    BookIsbnAlreadyExistsError,
+    BookNotFoundError,
+    BooksNotFoundError,
+)
+
+
+class ErrorMessageBookNotFound(BaseModel):
+    detail: str = Field(examples=[BookNotFoundError.message])
+
+
+class ErrorMessageBooksNotFound(BaseModel):
+    detail: str = Field(examples=[BooksNotFoundError.message])
+
+
+class ErrorMessageBookIsbnAlreadyExists(BaseModel):
+    detail: str = Field(examples=[BookIsbnAlreadyExistsError.message])
